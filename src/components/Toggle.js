452e35bf3b997db396a309366658ddad05999de2
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-04 14:58:04 -0500

    I made my first togglegit add .

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+    const [toggle, setToggle] = useState(true);
+
+  function handleClick() {
+    setToggle(!toggle);
+  }
+
+
+  return <button onClick={handleClick}>{toggle ? "On" : "Off"}</button>;
 }
 
 export default Toggle;
